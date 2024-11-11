4e8425713c690f2899d937000073bd256c48b05b
whiteapolo <darkpendraw@proton.me>
2024-11-11 16:51:30 +0200

    vector methods && minmaxheap

--- src/minMaxHeap.c
+++ src/minMaxHeap.c
@@ -56,6 +56,17 @@ bool hasGrandparent(size_t i)
 	return i > 2;
 }
 
+size_t getMaxIndex(const heap *h)
+{
+	if (h->size == 1)
+		return 0;
+	if (h->size == 2)
+		return 1;
+	if (h->cmp(h->vec[1], h->vec[2]) > 0)
+		return 1;
+	return 2;
+}
+
 void heapifyMaxUp(heap *h, size_t i)
 {
 	const size_t grandparent = parent(parent(i));
@@ -74,27 +85,14 @@ void heapifyMinUp(heap *h, size_t i)
 	}
 }
 
-void heapifyMaxDown(heap *h, size_t i)
+void heapifyUp(heap *h, size_t i)
 {
-
-}
-
-void heapifyMinDown(heap *h, size_t i)
-{
-
-}
-
-void heapPush(heap *h, void *data)
-{
-	const size_t i = (h->size++);
 	const size_t p = parent(i);
-
-	h->vec[i] = data;
 
 	if (i == 0)
 		return;
 
-	if (isMinLevel(h->size)) {
+	if (isMinLevel(i)) {
 		if (h->cmp(h->vec[i], h->vec[p] > 0)) {
 			swap(&h->vec[i], &h->vec[p], sizeof(void *));
 			heapifyMaxUp(h, p)
@@ -109,13 +107,94 @@ void heapPush(heap *h, void *data)
 			heapifyMaxUp(h, i);
 		}
 	}
+}
+
+void heapifyMaxDown(heap *h, size_t i)
+{
 
 }
 
-const void *heapPeek(const heap *h);
-void *heapPop(heap *h);
-size_t heapGetSize(const heap *h);
-bool heapIsEmpty(const heap *h);
-void heapShrinkToFit(heap *h);
-void heapClear(heap *h, void (*freeData)(void *));
-void heapFree(heap *h, void (*freeData)(void *));
+void heapifyMinDown(heap *h, size_t i)
+{
+
+}
+
+void heapifyDown(heap *h, size_t i)
+{
+	if (isMinLevel(i))
+		heapifyMinDown(h, i);
+	else
+		heapifyMaxDown(h, i);
+}
+
+void heapPush(heap *h, void *data)
+{
+	h->size++;
+	if (h->size > h->capacity) {
+		h->capacity *= 2;
+		h->vec = realloc(h->vec, sizeof(void *) * h->capacity);
+	}
+
+	h->vec[h->size - 1] = data;
+	heapifyUp(h, h->size - 1);
+
+
+}
+
+const void *heapPeekMin(const heap *h)
+{
+	return h->vec[0];
+}
+
+const void *heapPeekMax(const heap *h)
+{
+	return h->vec[getMaxIndex(h)];
+}
+
+void *heapPopMax(heap *h)
+{
+	size_t minI = getMaxIndex(h);
+	void *minData = h->vec[minI];
+	h->vec[minI] = h->vec[--h->size];
+	heapifyDown(h, minI);
+}
+
+void *heapPopMin(heap *h)
+{
+	void *minData = h->vec[0];
+	h->vec[0] = h->vec[--h->size];
+	heapifyMinDown(h, 0);
+	return minData;
+}
+
+size_t heapGetSize(const heap *h)
+{
+	return h->size;
+}
+
+bool heapIsEmpty(const heap *h)
+{
+	return h->size == 0;
+}
+
+void heapShrinkToFit(heap *h)
+{
+	h->capacity = h->size;
+	h->vec = realloc(h->vec, sizeof(void *) * h->capacity);
+}
+
+void heapClear(heap *h, void (*freeData)(void *))
+{
+	if (freeData != NULL)
+		for (size_t i = 0; i < h->size; i++)
+			freeData(h->vec[i]);
+	h->size = 0;
+}
+
+void heapFree(heap *h, void (*freeData)(void *))
+{
+	if (freeData != NULL)
+		for (size_t i = 0; i < h->size; i++)
+			freeData(h->vec[i]);
+	free(h->vec);
+}
