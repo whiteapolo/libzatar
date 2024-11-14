b32ebf8d128c02e3fb9c705a890837a95d1ba30a
whiteapolo <darkpendraw@proton.me>
2024-11-14 16:22:40 +0200

    int types

--- include/shared.h
+++ include/shared.h
@@ -5,6 +5,17 @@
 #include <stdlib.h>
 #include <stdbool.h>
 #include <string.h>
+#include <stdint.h>
+
+typedef int8_t   i8;
+typedef int16_t  i16;
+typedef int32_t  i32;
+typedef int64_t  i64;
+
+typedef uint8_t  u8;
+typedef uint16_t u16;
+typedef uint32_t u32;
+typedef uint64_t u64;
 
 #define ADDRESS(element) (&((typeof(element)){element}))
 #define DUP(value) memdup(ADDRESS(value), sizeof(value))
